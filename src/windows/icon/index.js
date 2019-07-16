8585939d7ec8410645ad8b8a2bec11d84902f6cb
chunfang740 <lcfhn168@gmail.com>
2019-07-17 01:38:57 +0800

    fullscreen

---
+++ src/windows/icon/index.js
@@ -0,0 +1,32 @@
+// 设置窗口图标
+/*
+icon
+ */
+const {app,BrowserWindow} = require('electron');
+function createWindow() {
+    win = new BrowserWindow({width:400,height:400,icon:'images/cziot48x481.ico'});
+    win.loadFile('index.html');
+    if(process.platform != 'darwin') {
+        win.setIcon('images/cziot48x481.ico')
+    }
+
+    win.on('closed',()=> {
+        console.log('closed');
+        win = null;
+    });
+}
+
+app.on('ready',createWindow);
+app.on('window-all-closed',()=>{
+    console.log('window-all-closed');
+    if(process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate',() =>{
+    console.log('activate');
+    if(win == null) {
+        createWindow();
+    }
+});

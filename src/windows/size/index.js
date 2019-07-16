280713ef0b71fb1a76df65a99c9cd7ea7b245254
chunfang740 <lcfhn168@gmail.com>
2019-07-16 20:27:18 +0800

    size

---
+++ src/windows/size/index.js
@@ -0,0 +1,25 @@
+const {app,BrowserWindow} = require('electron');
+function createWindow() {
+    win = new BrowserWindow({width:800,height:600 ,minWidth:200,minHeight:300,maxWidth:1000,maxHeight:800});
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+    win.on('closed',()=> {
+        console.log('closed');
+        win = null;
+    });
+}
+
+app.on('ready',createWindow);
+app.on('window-all-closed',()=>{
+    console.log('window-all-closed');
+    if(process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate',() =>{
+    console.log('activate');
+    if(win == null) {
+        createWindow();
+    }
+});
